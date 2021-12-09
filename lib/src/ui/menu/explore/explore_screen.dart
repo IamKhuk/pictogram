524789adf45309fc9f6f20c6328ddaa1b6c54f1d
IamKhuk <khukumovkhusan@gmail.com>
2021-12-09 23:30:32 +0500

    Staggered Grid view, Post Container

--- lib/src/ui/menu/explore/explore_screen.dart
+++ lib/src/ui/menu/explore/explore_screen.dart
@@ -1,7 +1,41 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:pictogram/src/theme/app_theme.dart';
+
+List<String> posts = [
+  'assets/images/post_07.png',
+  'assets/images/post_08.png',
+  'assets/images/post_09.png',
+  'assets/images/post_10.png',
+  'assets/images/post_11.png',
+  'assets/images/post_12.png',
+  'assets/images/post_13.png',
+  'assets/images/post_14.png',
+  'assets/images/post_15.png',
+  'assets/images/post_16.png',
+  'assets/images/post_17.png',
+  'assets/images/post_18.png',
+  'assets/images/post_19.png',
+  'assets/images/post_20.png',
+  'assets/images/post_07.png',
+  'assets/images/post_08.png',
+  'assets/images/post_09.png',
+  'assets/images/post_10.png',
+  'assets/images/post_11.png',
+  'assets/images/post_12.png',
+  'assets/images/post_13.png',
+  'assets/images/post_14.png',
+  'assets/images/post_15.png',
+  'assets/images/post_16.png',
+  'assets/images/post_17.png',
+  'assets/images/post_18.png',
+  'assets/images/post_19.png',
+  'assets/images/post_20.png',
+  'assets/images/post_16.png',
+  'assets/images/post_13.png',
+];
 
 class ExploreScreen extends StatefulWidget {
   @override
@@ -12,7 +46,7 @@ class _ExploreScreenState extends State<ExploreScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppTheme.white,
+      backgroundColor: AppTheme.bg2,
       appBar: AppBar(
         title: Text(
           ' Explore',
@@ -33,19 +67,40 @@ class _ExploreScreenState extends State<ExploreScreen> {
         backgroundColor: AppTheme.bg1,
         brightness: Brightness.light,
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(
-          100,
-          (index) {
-            return Center(
-              child: Text(
-                'Item $index',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-            );
-          },
+      body: StaggeredGridView.countBuilder(
+        padding: EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: 24,
         ),
+        itemCount: posts.length,
+        crossAxisCount: 3,
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 10),
+                  blurRadius: 75,
+                  spreadRadius: 0,
+                  color: Color(0xFF939393).withOpacity(0.1),
+                ),
+              ],
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.asset(
+                posts[index],
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+        staggeredTileBuilder: (index) => StaggeredTile.count(
+          index % 7 == 0 ? 2 : 1,
+          index % 7 == 0 ? 2 : 1,
+        ),
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 16,
       ),
     );
   }
---
+++ lib/src/widgets/post_container.dart
@@ -0,0 +1,243 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:pictogram/src/dialog/bottom_dialog.dart';
+import 'package:pictogram/src/model/post_model.dart';
+import 'package:pictogram/src/theme/app_theme.dart';
+import 'package:pictogram/src/ui/menu/home/home_screen.dart';
+import 'package:pictogram/src/ui/menu/home/likes_screen.dart';
+
+class PostContainer extends StatefulWidget {
+  final PostModel post;
+
+  PostContainer({required this.post});
+
+  @override
+  _PostContainerState createState() => _PostContainerState();
+}
+
+class _PostContainerState extends State<PostContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width - 48,
+      margin: EdgeInsets.only(bottom: 24),
+      padding: EdgeInsets.only(
+        top: 16,
+        bottom: 20,
+      ),
+      decoration: BoxDecoration(
+        color: AppTheme.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 10),
+            blurRadius: 75,
+            spreadRadius: 0,
+            color: Color(0xFF939393).withOpacity(0.1),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(width: 16),
+              Container(
+                height: 42,
+                width: 42,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(42),
+                  border: Border.all(color: AppTheme.blue),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(42),
+                  child: Image.asset(widget.post.pfp),
+                ),
+              ),
+              SizedBox(width: 20),
+              Expanded(
+                child: Text(
+                  widget.post.name,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: AppTheme.fontFamily,
+                    fontSize: 16,
+                    color: AppTheme.dark,
+                  ),
+                ),
+              ),
+              SvgPicture.asset('assets/icons/more.svg'),
+              SizedBox(width: 16),
+            ],
+          ),
+          SizedBox(height: 16),
+          Container(
+            height: 1,
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.symmetric(horizontal: 24),
+            color: AppTheme.gray.withOpacity(0.2),
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              SizedBox(width: 16),
+              Expanded(
+                child: Text(
+                  widget.post.comment,
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontFamily: AppTheme.fontFamily,
+                    fontWeight: FontWeight.normal,
+                    color: AppTheme.dark.withOpacity(0.8),
+                    height: 1.71,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 3,
+                ),
+              ),
+              SizedBox(width: 16),
+            ],
+          ),
+          SizedBox(height: 20),
+          Stack(
+            children: [
+              Container(
+                height: 170,
+                padding: EdgeInsets.only(
+                  left: 16,
+                  right: 16,
+                ),
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    autoPlay: false,
+                    viewportFraction: 1,
+                    scrollDirection: Axis.horizontal,
+                    enableInfiniteScroll: false,
+                  ),
+                  items: widget.post.images.map(
+                    (item) {
+                      return Container(
+                        height: 170,
+                        width: MediaQuery.of(context).size.width - 80,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: Image.asset(
+                            item,
+                            fit: BoxFit.fitWidth,
+                          ),
+                        ),
+                      );
+                    },
+                  ).toList(),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          widget.post.isLike = !widget.post.isLike;
+                          widget.post.isLike == true
+                              ? widget.post.likes.insert(
+                                  0,
+                                  me,
+                                )
+                              : widget.post.likes.removeAt(0);
+                        });
+                      },
+                      child: SvgPicture.asset(
+                        widget.post.isLike == false
+                            ? 'assets/icons/heart_i.svg'
+                            : 'assets/icons/heart_a.svg',
+                      ),
+                    ),
+                    SizedBox(width: 12),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return LikesScreen(
+                                users: widget.post.likes,
+                              );
+                            },
+                          ),
+                        );
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                          widget.post.likes.length.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontFamily: AppTheme.fontFamily,
+                            fontSize: 14,
+                            height: 1.71,
+                            color: AppTheme.dark.withOpacity(0.8),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    BottomDialog.showComments(
+                      context,
+                      widget.post.comments,
+                      (_comment) {
+                        setState(() {
+                          widget.post.comments.insert(0, _comment);
+                        });
+                      },
+                    );
+                  },
+                  child: Row(
+                    children: [
+                      SvgPicture.asset('assets/icons/comment.svg'),
+                      SizedBox(width: 12),
+                      Text(
+                        widget.post.comments.length.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontFamily: AppTheme.fontFamily,
+                          fontSize: 14,
+                          height: 1.71,
+                          color: AppTheme.dark.withOpacity(0.8),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Text(
+                widget.post.time.hour.toString() + " hours ago",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontFamily: AppTheme.fontFamily,
+                  fontSize: 14,
+                  height: 1.71,
+                  color: AppTheme.gray,
+                ),
+              ),
+              SizedBox(width: 16),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
