fb046868715ed25de1539e4aa60caa91e351ff40
IamKhuk <khukumovkhusan@gmail.com>
2022-01-01 22:52:10 +0500

    Story Screen

---
+++ lib/src/ui/menu/home/story_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class StroryScreen extends StatefulWidget {
+
+  @override
+  _StroryScreenState createState() => _StroryScreenState();
+}
+
+class _StroryScreenState extends State<StroryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
